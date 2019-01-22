0e75d075738d6a7b45adb38df9c89468d35250b7
Katharina Wiedmann <katie.wiedmann@gmx.de>
2019-01-22 16:48:08 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 21 09:27:16 2019
+
+@author: Katharina
+"""
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+#Asks for user input 
+@app.route("/generate")
+def generate():
+    return render_template("generate.html")
+
+
+#confirmation function then calls the confirmation html 
+@app.route("/confirmation", methods=['POST'])
+def confirmation():
+    formdata = request.form
+    postcode = formdata['postcode']
+    result = 'If postcode is okay, this will show.'
+    return render_template("confirmation.html", **locals())
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
